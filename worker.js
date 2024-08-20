fc087c489643acb2b85cd11db568f82f6aa48120
davidschipor <155432050+davidschipor@users.noreply.github.com>
2024-08-20 10:11:24 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 
-reverse = {"reverse.davidsch.online": {           // Domain of the cf worker
+reverse = {"davidsch.online": {           // Domain of the cf worker
         "protocol": "https",         // HTTP or HTTPS, protocol of the original site
         "host": "msisdn.telekom.ro",  // Target domain
         "replace": {                //Replace string for all json/html/text/javascript through this proxy
